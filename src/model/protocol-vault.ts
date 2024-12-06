c767db8012577c155051ce962fe8f76211f6edba
cipherZ <dev@cipherz.com>
2024-12-06 09:48:25 -0500

    finished simple smoketest for feedback

---
+++ src/model/protocol-vault.ts
@@ -0,0 +1,132 @@
+import assert from 'assert';
+import { Decimal } from 'decimal.js';
+import { Report } from '../util/report';
+
+const TOLERANCE = new Decimal(1e-8);
+
+export interface UserVault {
+    id: string;
+    debt: Decimal;
+    protocol_debt: Decimal;
+    sum_debt_t: Decimal;
+    collateral: Decimal;
+    protocol_collateral: Decimal;
+    sum_collateral_t: Decimal;
+}
+
+export class ProtocolVault {
+    private vaults: Map<string, UserVault> = new Map();
+
+    private aggregateVaultsBSD: Decimal = new Decimal(0);
+    private protocolVaultBalanceBSD: Decimal = new Decimal(0);
+
+    private sum_debt: Decimal = new Decimal(0);
+
+    report: Report;
+
+    // for reporting
+    private aggregateDistributionBSD: Decimal = new Decimal(0);
+
+    constructor(report: Report) {
+        this.report = report;
+    }
+
+    createVault(vaultId: string, debt: Decimal, collateral: Decimal) {
+
+        if (this.vaults.has(vaultId)) {
+            console.log(`Vault already exists`);
+            return;
+        }
+
+        const vault: UserVault = {
+            id: vaultId,
+            debt,
+            protocol_debt: new Decimal(0),
+            sum_debt_t: this.sum_debt,
+            collateral, //not used right now
+            protocol_collateral: new Decimal(0), // not used right now
+            sum_collateral_t: new Decimal(0) // not used right now
+        };
+
+        this.vaults.set(vaultId, vault);
+
+        this.aggregateVaultsBSD = this.aggregateVaultsBSD.plus(debt);
+
+        this.report.addAction(`Added vault: ${vaultId}`);
+    }
+
+    attributeProtocolVaultDebt(vault: UserVault) {
+        // See if there is any existing protocol debt that belongs to this vault that has not already been attributed to it
+        const existingProtocolBSD = this.calculateCurrentProtocolBSD(vault);
+
+        // If there is any existing protocol debt that has not been attributed to this vault, then we need to subtract it from the protocol vault balance
+        this.protocolVaultBalanceBSD = this.protocolVaultBalanceBSD.minus(existingProtocolBSD);
+
+        // add the new protocol debt to the aggregate vault bsd totals
+        this.aggregateVaultsBSD = this.aggregateVaultsBSD.plus(existingProtocolBSD);
+
+        // compound the attributed protocol debt to the vault
+        vault.protocol_debt = vault.protocol_debt.plus(existingProtocolBSD);
+
+        return vault;
+    }
+
+    repayDebt(amount: Decimal, vaultId: string) {
+
+        let vault = this.vaults.get(vaultId);
+        if (!vault) {
+            console.log(`Vault does not exist`);
+            return;
+        }
+
+        // attribute any existing protocol debt to the vault and adjust balances
+        vault = this.attributeProtocolVaultDebt(vault);
+
+        // update vault native debt balance and sum_t
+        this.vaults.set(vaultId, { ...vault, debt: vault.debt.minus(amount), sum_debt_t: this.sum_debt });
+
+        // reduce the aggregate debt totals for native vault debt
+        this.aggregateVaultsBSD = this.aggregateVaultsBSD.minus(amount);
+
+    }
+
+    private calculateCurrentProtocolBSD(vault: UserVault): Decimal {
+        const rewards = (vault.debt).mul(this.sum_debt.sub(vault.sum_debt_t));
+        return rewards;
+    }
+
+
+    redistribute(bsd: Decimal, sbtc: Decimal) {
+        this.sum_debt = this.sum_debt.add(bsd.div(this.aggregateVaultsBSD));
+        this.protocolVaultBalanceBSD = this.protocolVaultBalanceBSD.add(bsd);
+        this.aggregateDistributionBSD = this.aggregateDistributionBSD.add(bsd);
+
+    }
+
+    reconcile() {
+        console.log(`Reconciling vaults`);
+        let aggregateVaultDebt = new Decimal(0);
+        let aggregateProtocolVaultDebt = new Decimal(0);
+        this.vaults.forEach((vault) => {
+            vault = this.attributeProtocolVaultDebt(vault);
+            aggregateVaultDebt = aggregateVaultDebt.plus(vault.debt);
+            aggregateProtocolVaultDebt = aggregateProtocolVaultDebt.plus(vault.protocol_debt);
+        });
+        const aggregateDebt = aggregateVaultDebt.plus(aggregateProtocolVaultDebt);
+
+        // check that total vault debt is equal to the native debt and the attributed protocol debt
+        assert(aggregateDebt.minus(this.aggregateVaultsBSD).abs().lessThan(TOLERANCE), `aggregateDebt: ${aggregateDebt}, aggregateVaultsBSD: ${this.aggregateVaultsBSD} `);
+
+        // check that the total redistributed BSD is equal to the sum of the attributed protocol debt and the unattributed protocol debt
+        assert(this.aggregateDistributionBSD.minus(this.protocolVaultBalanceBSD.plus(aggregateProtocolVaultDebt)).abs().lessThan(TOLERANCE), `aggregateDistributionBSD: ${this.aggregateDistributionBSD}, protocolVaultBalanceBSD: ${this.protocolVaultBalanceBSD} aggregateProtocolVaultDebt: ${aggregateProtocolVaultDebt}`);
+
+        // check that the aggregate native debt + all distributions is equal the unattributed protocol debt + the aggregate native debt + attributed protocol debt
+        assert(aggregateVaultDebt.plus(this.aggregateDistributionBSD).minus(this.protocolVaultBalanceBSD.add(this.aggregateVaultsBSD)).abs().lessThan(TOLERANCE), `aggregateDebt: ${aggregateDebt}, aggregateDistributionBSD: ${this.aggregateDistributionBSD}, protocolVaultBalanceBSD: ${this.protocolVaultBalanceBSD} aggregateVaultsBSD: ${this.aggregateVaultsBSD}`);
+
+        this.vaults.forEach((vault) => {
+            this.report.addReconciliationRow({ provider: vault.id, vault_debt: vault.debt, vault_protocol_debt: vault.protocol_debt, protocolVaultBalanceBSD: this.protocolVaultBalanceBSD, aggregateVaultsBSD: this.aggregateVaultsBSD });
+        });
+    }
+}
+
+
--- src/scenario/smoketest.ts
+++ src/scenario/smoketest.ts
@@ -1,23 +1,21 @@
 import Decimal from 'decimal.js';
-import { StabilityPool } from "../model/stability-pool";
+import { ProtocolVault } from "../model/protocol-vault";
 import { Report } from '../util/report';
 
 export const executeScenario = (): any => {
 
     // Create an instance of StabilityPool
     const report = new Report();
-    const stabilityPool = new StabilityPool(report);
-    stabilityPool.addProvider('Alice');
-    stabilityPool.addProvider('Bob');
-    stabilityPool.depositStability(new Decimal(100), 'Alice');
-    stabilityPool.depositStability(new Decimal(50), 'Bob');
-    stabilityPool.withdrawStability(new Decimal(15), 'Bob');
-    stabilityPool.liquidate(new Decimal(50), new Decimal(.01));
-    stabilityPool.liquidate(new Decimal(25), new Decimal(.01));
-    stabilityPool.depositStability(new Decimal(100), 'Bob');
-    stabilityPool.liquidate(new Decimal(10), new Decimal(.01));
+    const stabilityPool = new ProtocolVault(report);
+    stabilityPool.createVault('Alice', new Decimal(10000), new Decimal(.15));
+    stabilityPool.createVault('Bob', new Decimal(20000), new Decimal(.30));
+    stabilityPool.redistribute(new Decimal(3000), new Decimal(.25))
+    stabilityPool.redistribute(new Decimal(3000), new Decimal(.25))
+    // stabilityPool.redistribute(new Decimal(2000), new Decimal(.015))
+    stabilityPool.repayDebt(new Decimal(1000), 'Alice');
+    stabilityPool.repayDebt(new Decimal(2000), 'Bob');
+    stabilityPool.redistribute(new Decimal(9000), new Decimal(.25))
     stabilityPool.reconcile();
-
     return { report }
 }
 
